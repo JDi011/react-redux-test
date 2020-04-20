f838cca8c2a8e0b9572b9b05c302dcd090f6b51c
jdi011 <jdi011@jdi011s-Mac-mini.local>
2020-04-20 05:39:51 +0530

    Get songlist

---
+++ src/components/SongDetail.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const SongDetail = ({ song }) => {
+    // console.log(props);
+    // props.song.title
+    // props.song.duration
+    if(!song){
+        return (
+            <div>
+                Select a song
+            </div>
+        );
+    };
+
+    return (
+        <div>
+        <h3>
+            Details for:
+        </h3>
+        <p>
+            Title: { song.title } <br/>
+            Duration {song.duration}
+        </p>
+            
+        </div>
+    );
+};
+
+const mapStateToProps = (state) => {
+    return { song : state.selectedSong }
+};
+
+export default connect(mapStateToProps)(SongDetail);
